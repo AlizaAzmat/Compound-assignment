5c9230cf6766bcb53167c242034afa780453e638
Aliza Azmat <35873624-itxmealizay@users.noreply.replit.com>
2024-08-25 16:44:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,168 @@
+# Compound Assignment Operator / Short Cut Operator
+
+# Compound Assignment Operators
+
+# Addition
+
+#1
+# a =25 
+# b=5 
+# b+=a # b = b + a
+# print(a) #25
+# print(b)  # 25 + 5 = 30
+
+#2
+# a =9
+# b=91 
+# b+=a  # b = b + a
+# print(a) #9 
+# print(b) # 91 + 9 = 100
+
+#3
+# a =110 
+# b=10 
+# b+=a  
+# print(a) #110 
+# print(b) # 10 + 110 = 120
+
+#4. 
+# a =49
+# b=1 
+# b+=a  
+# print(a) #49
+# print(b) # 1 + 49 = 50
+
+#5.
+# a =25 
+# a += 10  # a = a + 10
+#          # a = 25 + 10
+#         # a = 35
+# print(a)
+
+#Division 
+
+#1
+# a = 5
+# b= 25
+# b /= a # b = b / a
+#         # b = 25 / 5
+#         # b = 5.0
+# print(a) 
+# print(b)
+
+#2.  
+# a = 10
+# b = 90 
+# b -= a # b = b - a
+#       #  b = 90 - 10 
+#         # b = 80
+# print(a)  # 10
+# print(b) # 80
+
+#3. 
+# a =2
+# b=10 
+# b  /= a   # b = b / a
+#           # b = 10 / 2
+#           # b = 5.0
+# print(a) #2
+# print(b) # 5.0
+
+#4 
+# a =49
+# b=7
+# b  /= a # b = b / a
+#         # b = 49 / 7
+#         # b = 7.0
+# print(a)  # 49
+# print(b)  # 7.0
+
+
+#5 
+# a = 35
+# a  /= 5    # a  =  a / 5
+#           # a = 35 / 5
+#           # a = 7.0
+# print(a) #7.0
+
+
+# Substraction
+
+
+# #1
+# a =25 
+# b=5 
+# b-=a # b = b - a
+#     # b = 5 - 25
+#     # b = -20
+# print(a) #25
+# print(b)  # -20
+
+#2
+# a =9
+# b=91 
+# b -= a  # b = b - a
+#         # b = 91 - 9
+#         # b = 82
+# print(a) #9 
+# print(b) # 82
+
+#3
+# a = 110 
+# b = 10 
+# b -= a  
+# print(a) # 110
+# print(b) # -100
+
+#4. 
+# a = 49
+# b = 1 
+# b -= a  
+# print(a) #49
+# print(b) # 1 - 49 = -48
+
+#5.
+# a =25 
+# a -= 10  # a = a - 10
+#          # a = 25 - 10
+#         # a = 15
+# print(a) # 15
+
+
+# Multiplication
+
+#1. 
+# a =3
+# a *= 5   # a = a * 5
+#         # 3 * 5 = 15
+#         # a = 15
+# print(a) #15
+
+#2. 
+# a = 4
+# a *= 2   # a = a * 2
+#         # 4 * 2 = 8
+#         # a = 8
+# print(a) #8
+
+#3. 
+# a = 5
+# a *= 12   # a = a * 12
+#         # 5 * 12 = 60
+#         # a = 60
+# print(a) #60
+
+#4. 
+# a = 6
+# a *= 6   # a = a * 6
+#         # 6 * 6 = 36
+#         # a = 36
+# print(a) #36
+
+#5. 
+# a = 9
+# a *= 10  # a = a * 10
+#         # 9 * 10 = 90
+#         # a = 90
+# print(a) #90
+
